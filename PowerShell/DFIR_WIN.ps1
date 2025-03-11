d6d18bc7475261a6a75778dbd9432bd768153ab4
odubdami429 <odubdami429@gmail.com>
2025-03-11 05:26:42 -0400

    Anonymizing identifying details

--- PowerShell/DFIR_WIN.ps1
+++ PowerShell/DFIR_WIN.ps1
@@ -1,5 +1,4 @@
 #======================================================================================
-#Author: Dami Odubanjo
 #This script collects various DFIR artifact from a Windows Endpoint by saving the output of various commands to a txt file 
 #Once the the script is done the output files will be placed in C:\Temp\DFIR_Output
 #======================================================================================
