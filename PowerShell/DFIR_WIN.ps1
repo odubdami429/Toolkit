c3cb01ccf505a199ce13dc3a0e33e5eb828e2858
odubdami429 <odubdami429@gmail.com>
2025-03-11 11:01:42 -0400

    Added timezones outputed files

--- PowerShell/DFIR_WIN.ps1
+++ PowerShell/DFIR_WIN.ps1
@@ -17,16 +17,19 @@ query user | Out-File -append "C:\Temp\DFIR_Output\system_info.txt";
 whoami /all | Out-File -append "C:\Temp\DFIR_Output\system_info.txt";
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\system_info.txt";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\system_info.txt";
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\system_info.txt"
 
 #Collect list of Running Processes
 tasklist /v | Out-File "C:\Temp\DFIR_Output\running_processes.txt";
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\running_processes.txt";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\running_processes.txt"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\running_processes.txt"
 
 #Collect list of scheduled task
 Get-ChildItem C:\Windows\System32\Tasks | Out-File "C:\Temp\DFIR_Output\scheduled_task.txt";
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\scheduled_task.txt";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\scheduled_task.txt"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\scheduled_task.txt"
 
 #Collect list of system-level installed apps 
 Get-ItemProperty `
@@ -37,6 +40,7 @@ Get-ItemProperty `
 | Export-Csv -Path "C:\Temp\DFIR_Output\system_level_installed_apps.csv" -NoTypeInformation
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\system_level_installed_apps.csv";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\system_level_installed_apps.csv"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\system_level_installed_apps.csv"
 
 #Collect list of user-level installed apps 
 # Gather all user SIDs from HKEY_USERS except .DEFAULT and *_Classes
@@ -72,7 +76,7 @@ foreach ($sid in $allUserSIDs) {
 $allUninstalls | Export-Csv -Path "C:\Temp\DFIR_Output\user_level_installed_apps.csv" -NoTypeInformation
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\user_level_installed_apps.csv";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\user_level_installed_apps.csv"
-
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\user_level_installed_apps.csv"
 
 #==========================================
 #Collecting Networking Information
@@ -82,16 +86,19 @@ Get-Date | Out-File -append "C:\Temp\DFIR_Output\user_level_installed_apps.csv"
 Get-NetUDPEndpoint  | Select-Object LocalAddress,LocalPort,CreationTime,OwningProcess,@{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}} | ft -auto | Out-File "C:\Temp\DFIR_Output\udp_connections.txt";
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\udp_connections.txt";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\udp_connections.txt"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\udp_connections.txt"
 
 #Collect list of TCP connections
 Get-NetTCPConnection |  select-object LocalAddress,LocalPort,RemoteAddress,RemotePort,State,CreationTime,OwningProcess, @{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}} | ft -auto | Out-File "C:\Temp\DFIR_Output\tcp_connections.txt";
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\tcp_connections.txt";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\tcp_connections.txt"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\tcp_connections.txt"
 
 #Collect list of Wifi Profiles
 netsh wlan show profiles | Out-File "C:\Temp\DFIR_Output\wifi_profiles.txt";
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\wifi_profiles.txt";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\wifi_profiles.txt"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\wifi_profiles.txt"
 
 #Collect list of all firewall rules on the system
 $fwRules = Get-NetFirewallRule | ForEach-Object {
@@ -120,6 +127,7 @@ $fwRules = Get-NetFirewallRule | ForEach-Object {
 $fwRules | Export-Csv -Path "C:\Temp\DFIR_Output\firewall_settings.csv" -NoTypeInformation
 "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\firewall_settings.csv";
 Get-Date | Out-File -append "C:\Temp\DFIR_Output\firewall_settings.csv"
+Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\firewall_settings.csv"
 
 
 #==========================================
@@ -152,6 +160,7 @@ if ($Manufacturer -like "*Amazon EC2*") {
     Get-ChildItem D:\Users -Recurse | Out-File "C:\Temp\DFIR_Output\user_D_drive_files.txt";
     "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\user_D_drive_files.txt";
     Get-Date | Out-File -append "C:\Temp\DFIR_Output\user_D_drive_files.txt"
+    Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\user_D_drive_files.txt"
 
     $d_drive_users = (Get-ChildItem D:\Users).Name
 
@@ -185,6 +194,7 @@ else {
     Get-ChildItem C:\Users -Recurse | Out-File "C:\Temp\DFIR_Output\user_C_drive_files.txt";
     "`n`nDate of Artifact Collection:" | Out-File -append "C:\Temp\DFIR_Output\user_C_drive_files.txt";
     Get-Date | Out-File -append "C:\Temp\DFIR_Output\user_C_drive_files.txt"
+    Get-Timezone | Out-File -append "C:\Temp\DFIR_Output\user_C_drive_files.txt"
     
     #Copy over the Powershell history log file and chrome history file for all user profiles on the endpoint
     $c_drive_users = (Get-ChildItem C:\Users).Name
